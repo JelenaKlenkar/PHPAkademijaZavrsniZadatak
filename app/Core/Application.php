cfdd0fbba6d5099b4953f4cfa7e36de69fa138f4
JelenaKlenkar <jelena.klenkar@gmail.com>
2020-09-24 21:56:10 +0200

    added basic MVC arhitecture

---
+++ app/Core/Application.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Core;
+
+class Application
+{
+    protected $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function run(): string
+    {
+        return $this->router->match($_SERVER['PATH_INFO'] ?? '/');
+    }
+}
